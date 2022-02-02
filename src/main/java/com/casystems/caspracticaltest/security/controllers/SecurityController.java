025706eb39567c770ae7884659f3cfe325864dde
Luis Garcia <lgarciaag89@gmail.com>
2022-02-01 23:40:51 -0800

    fix usses to SecurityController class

--- src/main/java/com/casystems/caspracticaltest/security/controllers/SecurityController.java
+++ src/main/java/com/casystems/caspracticaltest/security/controllers/SecurityController.java
@@ -5,9 +5,10 @@ import com.casystems.caspracticaltest.system.models.Role;
 import com.casystems.caspracticaltest.system.models.User;
 import com.casystems.caspracticaltest.system.services.RoleService;
 import com.casystems.caspracticaltest.system.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +20,22 @@ import java.util.*;
 @Controller
 public class SecurityController {
 
-    @Autowired
-    private RoleService roleService;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private UserValidator userValidator;
+    private static final String SIGUP = "sigup";
+
+    private final RoleService roleService;
+    private final UserService userService;
+    private final  UserValidator userValidator;
+
+    @Inject
+    public SecurityController(RoleService roleService, UserService userService, UserValidator userValidator) {
+        Assert.notNull(roleService,"RoleService most not be null");
+        Assert.notNull(userService,"UserService most not be null");
+        Assert.notNull(userValidator,"UserValidator most not be null");
+        this.roleService = roleService;
+        this.userService = userService;
+        this.userValidator = userValidator;
+    }
+
 
 
     @GetMapping({"/","/login"})
@@ -38,24 +49,22 @@ public class SecurityController {
         User user  = new User();
         user.setRoles(new HashSet<>(roles));
         model.addAttribute("user", user);
-        return "sigup";
+        return SIGUP;
     }
 
     @PostMapping("/sigup")
     public String registration(@ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
         userValidator.validate(user, bindingResult);
 
-        if (bindingResult.hasErrors()) {
-            return "sigup";
+        if(!bindingResult.hasErrors()) {
+            userService.saveOrUpdateUser(user);
+            ArrayList<Role> roles = roleService.getNotAdminRoles();
+            User newUser = new User();
+            newUser.setRoles(new HashSet<>(roles));
+            model.addAttribute("user", newUser);
+            model.addAttribute("message", "Successful!");
         }
-
-        userService.saveOrUpdateUser(user);
-        ArrayList<Role> roles = roleService.getNotAdminRoles();
-        User newUser  = new User();
-        newUser.setRoles(new HashSet<>(roles));
-        model.addAttribute("user", newUser);
-        model.addAttribute("message", "Successful!");
-        return "sigup";
+        return SIGUP;
     }
 
 
